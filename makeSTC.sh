cc66d7bd1cc386ac6d7581e22eaabdaa1b5a3da6
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2011-04-18 09:31:54 -0400

    fixed errors in makeSTC

--- makeSTC.sh
+++ makeSTC.sh
@@ -9,30 +9,30 @@ mkdir stc
 
 ###Unmorphed##
 
-##This is set 4, the unrelated filler in LP
-mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 4 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c4-spm.stc --smooth 5 --spm
+##This is set 3, the unrelated filler in LP
+mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 3 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c3-spm.stc --smooth 5 --spm
 
 ##This is sets 1 and 2, targets of interest in LP
 mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c1-spm.stc --smooth 5 --spm
 mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c2-spm.stc --smooth 5 --spm
 
-##This is sets 6 and 7, targets of interest in HP
+##This is sets 1 and 2, targets of interest in HP
 mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c1-spm.stc --smooth 5 --spm
 mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c2-spm.stc --smooth 5 --spm
 
 
 ##Morphed##
 
-##This is set 4, the unrelated filler in LP
-mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 4 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c4-spm.stc --smooth 5 --spm --morph fsaverage
+##This is set 3, the unrelated filler in LP
+mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 3 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c3M-spm.stc --smooth 5 --spm --morph fsaverage
 
 ##This is sets 1 and 2, targets of interest in LP
-mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c1-spm.stc --smooth 5 --spm --morph fsaverage
-mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c2-spm.stc --smooth 5 --spm --morph fsaverage
+mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c1M-spm.stc --smooth 5 --spm --morph fsaverage
+mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c2M-spm.stc --smooth 5 --spm --morph fsaverage
 
-##This is sets 6 and 7, targets of interest in HP
-mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 6 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c1-spm.stc --smooth 5 --spm --morph fsaverage
-mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 7 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c2-spm.stc --smooth 5 --spm --morph fsaverage
+##This is sets 1 and 2, targets of interest in HP
+mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c1M-spm.stc --smooth 5 --spm --morph fsaverage
+mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c2M-spm.stc --smooth 5 --spm --morph fsaverage
 	
 
 ########FIX GROUP ON ALL FILES########
