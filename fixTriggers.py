0f5a7fe0216d7ef23dc73f8808fdaf95260e6f66
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2011-04-28 12:32:47 -0400

    changed to new Baleen naming conventions in fixTriggers.py

--- fixTriggers.py
+++ fixTriggers.py
@@ -46,14 +46,16 @@ def fixTriggers(subjID):
 	
 	writeOutput.writeTable(outFile,data)
 	
-	#BALEEN
 	
-	RUNS = ['1', '2', '3', '4', '5', '6', '7', '8']
+	###############################################
+	#BALEENLP
+	
+	RUNS = ['1', '2', '3', '4']
 	
 	for x in RUNS:
 	
-		inFile = 'eve/'+subjID+'_BaleenRun'+x+'.eve'
-		outFile = 'eve/'+subjID+'_BaleenRun'+x+'Mod.eve'
+		inFile = 'eve/'+subjID+'_BaleenLPRun'+x+'.eve'
+		outFile = 'eve/'+subjID+'_BaleenLPRun'+x+'Mod.eve'
 		
 		print inFile
 		data = readInput.readTable(inFile)
@@ -72,7 +74,8 @@ def fixTriggers(subjID):
 			row[0] = str(int(round(trueSample,0)))
 			row[1] = str(round(trueTime,3))
 			
-			if trigger == '11' or trigger == '12':   ##change the target to '111' or '112'
+			##This part fixes the coding for the probe primes. Originally the trigger 11 was sent for the target when the prime was a probe. This recodes the target as 111 and codes the prime itself as 11
+			if trigger == '11':   ##change the target to '111'
 				row[3] = '1'+trigger
 				#print row
 				
@@ -94,11 +97,66 @@ def fixTriggers(subjID):
 		firstRow[0] = firstSample
 		firstRow[1] = firstTime
 	
-	
-		
 		#SB moved this to end of biggest loop (otherwise writing file every row iteration)
 		writeOutput.writeTable(outFile, data)
+
+
+	###############################################
+	#BALEENHP
 	
+	RUNS = ['1', '2', '3', '4']
+	
+	for x in RUNS:
+	
+		inFile = 'eve/'+subjID+'_BaleenHPRun'+x+'.eve'
+		outFile = 'eve/'+subjID+'_BaleenHPRun'+x+'Mod.eve'
+		
+		print inFile
+		data = readInput.readTable(inFile)
+		
+		firstRow = data[0]
+		firstSample = firstRow[0]
+		firstTime = firstRow[1]
+	
+		rowCount = 0
+		for row in data:
+			trigger = row[3]
+			time = row[1]
+			sampleRate = float(row[0])/float(row[1])
+			trueSample = float(row[0]) + 19
+			trueTime = trueSample/sampleRate
+			row[0] = str(int(round(trueSample,0)))
+			row[1] = str(round(trueTime,3))
+			
+			if trigger == '12':   ##change the target to '112'
+				row[3] = '1'+trigger
+				#print row
+				
+				for i in [1,2,3]:                 ## look through the previous three events to try to find the prime
+					compRow = data[rowCount-i]
+					#print 'Comparison', compRow
+	
+					compTrigger = compRow[3]
+					compSOA = float(row[1])-float(compRow[1])
+					if compTrigger == '14' and ( compSOA > .580 and compSOA < .620 ):  ##check for a prime with correct SOA
+						compRow[3] = trigger
+						#print 'new ', compRow, row
+						break
+				
+			rowCount +=1
+		
+		##Undo the timing change for the first row in file because this row indicates the beginning of the scan, not a visual event, so it shouldn't be changed
+	
+		firstRow[0] = firstSample
+		firstRow[1] = firstTime
+	
+		#SB moved this to end of biggest loop (otherwise writing file every row iteration)
+		writeOutput.writeTable(outFile, data)
+
+
+
+
+
 	#SB	
 	###MaskedMM
 	
