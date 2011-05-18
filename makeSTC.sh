e0f340fa77c14e8b35b992fd1bfa9b6ebe7c43a4
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2011-05-18 16:57:08 -0400

    added more mne files to makeSTC

--- makeSTC.sh
+++ makeSTC.sh
@@ -36,9 +36,19 @@ mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-av
 mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c1M-spm.stc --smooth 5 --spm --morph fsaverage
 mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c2M-spm.stc --smooth 5 --spm --morph fsaverage
 
+
 ###MNE####
 ##This is set 3, the unrelated filler in LP
 mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 3 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c3M-mne.stc --smooth 5 --morph fsaverage
+
+##This is sets 1 and 2, targets of interest in LP
+mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c1M-mne.stc --smooth 5 --morph fsaverage
+mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c2M-mne.stc --smooth 5 --morph fsaverage
+
+##This is sets 1 and 2, targets of interest in HP
+mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c1M-mne.stc --smooth 5 --morph fsaverage
+mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c2M-mne.stc --smooth 5 --morph fsaverage
+
 
 ########FIX GROUP ON ALL FILES########
 chgrp -R lingua .
