984eab9230d7498d57485ddbf216bd1fde374b88
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2011-08-25 14:27:48 -0400

    modified plotting script for baleen

--- plot_label_source_activations.py
+++ plot_label_source_activations.py
@@ -16,10 +16,25 @@ import mne
 
 import numpy as np
 
+hem = 'lh'
+exp = 'HP'
+
 data_path = '/cluster/kuperberg/SemPrMM/MEG/results/source_space/ga_stc'
-stc_fname = data_path + '/diff/ga_BaleenLP_All_c2-c1M_n24-spm-lh.stc'
-label = 'BaleenHP_c1_c2_350-450_cluster1-lh'
+stc_fname = data_path + '/diff/ga_Baleen'+exp+'_All_c2-c1M_n24-spm-'+hem+'.stc'
+
+#label = 'BaleenHP_c1_c2_350-450_cluster0-'+hem
+label = 'S_temporal_sup-'+hem
 label_fname = data_path + '/label/%s.label' % label
+
+#label = 'G_front_inf-Triangul-'+hem
+#label = 'G_front_inf-Opercular-'+hem
+#label = 'G_front_inf-Orbital-'+hem
+#label = 'G_temp_sup-Lateral-'+hem
+#label = 'G_temporal_middle-'+hem
+#label = 'Pole_temporal-'+hem
+#label = 'S_temporal_sup-'+hem
+#label = 'G_pariet_inf-Angular-'+hem
+
 
 values, times, vertices = mne.label_time_courses(label_fname, stc_fname)
 
@@ -31,8 +46,17 @@ print "Number of vertices : %d" % len(vertices)
 
 # View source activations
 import pylab as pl
-pl.plot(times, values.T)
+pl.plot([None], [None])
+if exp == 'LP':
+	pl.plot(times, values.T,color='b',linewidth=4)
+if exp == 'HP':
+	pl.plot(times, values.T,color='b',linewidth=4,linestyle='dotted')
+	pl.legend(('low-proportion','high-proportion'))
+pl.plot(times,values.T*0,color='k')
+pl.ylim([-1,1])
+pl.xlim([-.1,.51])
 pl.xlabel('time (ms)')
 pl.ylabel('Source amplitude')
 pl.title('Activations in Label : %s' % label)
+
 pl.show()
