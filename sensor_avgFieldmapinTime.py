45f68b77f4af035aea1da810d61e410368b13128
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2011-08-26 09:20:06 -0400

    fix typos

--- sensor_avgFieldmapinTime.py
+++ sensor_avgFieldmapinTime.py
@@ -1,6 +1,8 @@
 import numpy as np
 import mne
 
+##script for creating average field map
+
 evokedCrop = mne.fiff.read_evoked('ga_BaleenLP_All_meg-n24-goodC-ave.fif',setno=4)
 evokedCrop.crop(tmin=0.35, tmax=0.45)
 data = evokedCrop.data.mean(axis=1)
