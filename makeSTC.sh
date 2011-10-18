af54369f58ee450ae923fe1c5210680ccb959b58
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2011-10-18 16:01:57 -0400

    changing STC files to compute in makeSTC.sh

--- makeSTC.sh
+++ makeSTC.sh
@@ -54,26 +54,26 @@ mkdir stc
 # mne_make_movie --inv $1_ATLLoc-ave-7-meg-inv.fif --meas $1_ATLLoc-ave.fif --set 3 --bmin -100 --bmax -.01 --stc stc/$1_ATLLoc_c3M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
 
 ###############MASKEDMM#####################
-mne_make_movie --inv $1_MaskedMM_All-ave-7-meg-inv.fif --meas $1_MaskedMM_All-ave.fif --set 1 --bmin -100 --bmax 0 --stc stc/$1_MaskedMM_All_c1M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
+#mne_make_movie --inv $1_MaskedMM_All-ave-7-meg-inv.fif --meas $1_MaskedMM_All-ave.fif --set 1 --bmin -100 --bmax 0 --stc stc/$1_MaskedMM_All_c1M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
 
-mne_make_movie --inv $1_MaskedMM_All-ave-7-meg-inv.fif --meas $1_MaskedMM_All-ave.fif --set 2 --bmin -100 --bmax 0 --stc stc/$1_MaskedMM_All_c2M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
+#mne_make_movie --inv $1_MaskedMM_All-ave-7-meg-inv.fif --meas $1_MaskedMM_All-ave.fif --set 2 --bmin -100 --bmax 0 --stc stc/$1_MaskedMM_All_c2M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
 
-mne_make_movie --inv $1_MaskedMM_All-ave-7-meg-inv.fif --meas $1_MaskedMM_All-ave.fif --set 3 --bmin -100 --bmax 0 --stc stc/$1_MaskedMM_All_c3M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
+#mne_make_movie --inv $1_MaskedMM_All-ave-7-meg-inv.fif --meas $1_MaskedMM_All-ave.fif --set 3 --bmin -100 --bmax 0 --stc stc/$1_MaskedMM_All_c3M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
 
 ###############BALEEN#######################
 # This is set 3, the unrelated filler in LP
-# mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 3 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c3M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
+mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 3 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c3M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
 # 
 # This is sets 1 and 2, targets of interest in LP
-# mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c1M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
-# mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c2M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
+#mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c1M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
+#mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c2M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
 # 
 # This is sets 1 and 2, targets of interest in HP
 # mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c1M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
 # mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c2M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
 # 
 # This is sets 3 and 4, related (half) and unrelated fillers in HP
-# mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 3 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c3M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
+mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 3 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c3M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
 # mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 4 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c4M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
 # 
 # #############AXCPT#######################
