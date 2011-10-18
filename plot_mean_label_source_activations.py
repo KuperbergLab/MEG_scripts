f2129f158e11a79a0dd3b2ca1695dee01401bca9
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2011-10-18 16:01:29 -0400

    changing parameters in plot_mean_label_source_activations.py

--- plot_mean_label_source_activations.py
+++ plot_mean_label_source_activations.py
@@ -18,15 +18,15 @@ import argparse
 
 ####Plotting Parameters####
 xmin,xmax = [-100, 501]
-ymin,ymax = [-.5, 1.5]
+ymin,ymax = [-.5, 8]
 lWidth = 4
 
 color1 = 'b'
 color2 = 'b'
 lineStyle1 = 'dotted'
 lineStyle2 = 'solid'
-lineLabel1 = 'related'
-lineLabel2 = 'unrelated'
+lineLabel1 = 'LP left'
+lineLabel2 = 'LP right'
 
 
 ####################################
@@ -44,10 +44,12 @@ parser.add_argument('set2',type=str)
 args=parser.parse_args()
 
 data_path = '/cluster/kuperberg/SemPrMM/MEG/results/source_space/ga_stc'
-stc1_fname = data_path + '/diff/Baleen/ga_'+args.protocol1+'_All_c'+args.set2+'-c'+args.set1+'M_n24-spm-'+args.hem1+'.stc'
-stc2_fname = data_path + '/diff/Baleen/ga_'+args.protocol2+'_All_c'+args.set2+'-c'+args.set1+'M_n24-spm-'+args.hem2+'.stc'
-#stc1_fname = data_path + '/single_condition/ga_'+args.protocol1+'_All_c'+args.set1+'M_n20-spm-'+args.hem1+'.stc'
-#stc2_fname = data_path + '/single_condition/ga_'+args.protocol2+'_All_c'+args.set2+'M_n20-spm-'+args.hem2+'.stc'
+#stc1_fname = data_path + '/diff/Baleen/ga_'+args.protocol1+'_All_c'+args.set2+'-c'+args.set1+'M_n24-spm-'+args.hem1+'.stc'
+#stc2_fname = data_path + '/diff/Baleen/ga_'+args.protocol2+'_All_c'+args.set2+'-c'+args.set1+'M_n24-spm-'+args.hem2+'.stc'
+#stc1_fname = data_path + '/single_condition/ga_'+args.protocol1+'_All_c'+args.set1+'M_n24-spm-'+args.hem1+'.stc'
+#stc2_fname = data_path + '/single_condition/ga_'+args.protocol2+'_All_c'+args.set2+'M_n24-spm-'+args.hem2+'.stc'
+stc1_fname = data_path + '/single_condition/ga_'+args.protocol1+'_AllUnrelated_c'+args.set1+'M_n24-spm-'+args.hem1+'.stc'
+stc2_fname = data_path + '/single_condition/ga_'+args.protocol2+'_AllUnrelated_c'+args.set2+'M_n24-spm-'+args.hem2+'.stc'
 
 
 label1 = args.label1+'-'+args.hem1
