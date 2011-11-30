2ab09e11ab245613de0cc2b8f42bd9e42a57a1ff
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2011-11-30 14:36:45 -0500

    changed makeSTC to save stcs to exp subdirectories

--- makeSTC.sh
+++ makeSTC.sh
@@ -25,6 +25,8 @@ setenv SUBJECT $1
 cd /cluster/kuperberg/SemPrMM/MEG/data/$1/ave_projon
 
 mkdir stc
+mkdir stc/BaleenLP_All
+mkdir stc/BaleenHP_All
 
 #####Make stc files to subtract in Matlab####
 
@@ -62,19 +64,23 @@ mkdir stc
 
 ###############BALEEN#######################
 # This is set 3, the unrelated filler in LP
-mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 3 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c3M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
+mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 3 --bmin -100 --bmax -.01 --stc stc/BaleenLP_All/$1_BaleenLP_All_c3M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
 # 
 # This is sets 1 and 2, targets of interest in LP
-#mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c1M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
-#mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c2M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
+mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/BaleenLP_All/$1_BaleenLP_All_c1M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
+mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/BaleenLP_All/$1_BaleenLP_All_c2M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
 # 
 # This is sets 1 and 2, targets of interest in HP
-# mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c1M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
-# mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c2M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
-# 
+mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/BaleenHP_All/$1_BaleenHP_All_c1M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
+mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/BaleenHP_All/$1_BaleenHP_All_c2M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
+
+# This is sets 1 and 2, targets of interest in HP, meg-eeg
+#mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-eeg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_meeg_c1M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
+#mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-eeg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_meeg_c2M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
+
 # This is sets 3 and 4, related (half) and unrelated fillers in HP
-mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 3 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c3M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
-# mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 4 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c4M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
+mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 3 --bmin -100 --bmax -.01 --stc stc/BaleenHP_All/$1_BaleenHP_All_c3M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
+mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 4 --bmin -100 --bmax -.01 --stc stc/BaleenHP_All/$1_BaleenHP_All_c4M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
 # 
 # #############AXCPT#######################
 # mne_make_movie --inv $1_AXCPT_All-ave-7-meg-inv.fif --meas $1_AXCPT_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/$1_AXCPT_All_c1M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
@@ -85,19 +91,19 @@ mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-av
 # 
 # 
 # #####################################################################
-# 
 # #MNE####
+
 # This is set 3, the unrelated filler in LP
-# mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 3 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c3M-mne.stc --smooth 7 --morph fsaverage >>& $log
-# 
+mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 3 --bmin -100 --bmax -.01 --stc stc/BaleenLP_All/$1_BaleenLP_All_c3M-mne.stc --smooth 7 --morph fsaverage >>& $log
+ 
 # This is sets 1 and 2, targets of interest in LP
-# mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c1M-mne.stc --smooth 7 --morph fsaverage >>& $log
-# mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/$1_BaleenLP_All_c2M-mne.stc --smooth 7 --morph fsaverage >>& $log
-# 
+mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/BaleenLP_All/$1_BaleenLP_All_c1M-mne.stc --smooth 7 --morph fsaverage >>& $log
+mne_make_movie --inv $1_BaleenLP_All-ave-7-meg-inv.fif --meas $1_BaleenLP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/BaleenLP_All/$1_BaleenLP_All_c2M-mne.stc --smooth 7 --morph fsaverage >>& $log
+ 
 # This is sets 1 and 2, targets of interest in HP
-# mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c1M-mne.stc --smooth 7 --morph fsaverage >>& $log
-# mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c2M-mne.stc --smooth 7 --morph fsaverage >>& $log
-# 
+mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 1 --bmin -100 --bmax -.01 --stc stc/BaleenHP_All/$1_BaleenHP_All_c1M-mne.stc --smooth 7 --morph fsaverage >>& $log
+mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 2 --bmin -100 --bmax -.01 --stc stc/BaleenHP_All/$1_BaleenHP_All_c2M-mne.stc --smooth 7 --morph fsaverage >>& $log
+
 # This is sets 3 and 4, related and unrelated fillers in HP
 # mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 3 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c3M-mne.stc --smooth 7 --morph fsaverage >>& $log
 # mne_make_movie --inv $1_BaleenHP_All-ave-7-meg-inv.fif --meas $1_BaleenHP_All-ave.fif --set 4 --bmin -100 --bmax -.01 --stc stc/$1_BaleenHP_All_c4M-mne.stc --smooth 7 --morph fsaverage >>& $log
