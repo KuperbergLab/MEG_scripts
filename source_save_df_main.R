c65fff2f4d9434e36305850a2230229f7e9c1e14
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2012-01-11 11:55:27 -0500

    made source_save_df_main.R generic for all paradigms

--- source_save_df_main.R
+++ source_save_df_main.R
@@ -5,10 +5,6 @@ fileName <- paste(filePath,filePrefix,t1,"-",t2,".txt",sep="")
 buffer<-read.table(fileName)
 sourceData.all<-data.frame(subj=factor(buffer$V1),cond=factor(buffer$V2),roi=factor(buffer$V3),hemCode=factor(buffer$V4),amp=buffer$V5)
 
-sourceData.all$prime<-factor(sourceData.all$cond,exclude=NULL);
-levels(sourceData.all$prime)<-c("rel","unrel","rel","unrel");
-sourceData.all$prop<-factor(sourceData.all$cond,exclude=NULL);
-levels(sourceData.all$prop)<-c("hi","hi","lo","lo");
 
 outFile <- paste(filePath,filePrefix,t1,"-",t2,".df",sep="")
 save(sourceData.all,file=outFile)
