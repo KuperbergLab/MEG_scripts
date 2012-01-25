3f189439fcf03339a9018f411d7f23011baf335e
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2012-01-25 09:19:13 -0500

    Modified to run on ya-AXCPT data(Before modifying results directory structure).

--- MEGArtReject.sh
+++ MEGArtReject.sh
@@ -1,39 +1,43 @@
 #!/bin/csh -f
-#usage: MEGArtReject.sh SubjType
-#usage: subjType: sc ac or ya (in Lower case)
+#usage: MEGArtReject.sh SubjType 
+#usage: subjType: sc ac or ya (in Lower case) 'ATLLoc' 'BaleenHP' 'BaleenLP' 'MaskedMM' 'AXCPT'- (only in the case of ya)
 
 echo 'Clearing existing results from MEG/data/subject/ave_projon/logs folders'
 cd /autofs/cluster/kuperberg/SemPrMM/MEG/data/
    foreach i ( $1* )
-     echo $i
+     
      cd /autofs/cluster/kuperberg/SemPrMM/MEG/data/$i/ave_projon/logs/
-        rm {$i}_MEGArtRejectATLLoc
-        rm {$i}_MEGArtRejectBaleenHP
-        rm {$i}_MEGArtRejectBaleenLP
-        rm {$i}_MEGArtRejectMaskedMM
-        rm {$i}_MEGArtReject-BadChanATLLoc
-        rm {$i}_MEGArtReject-BadChanBaleenHP
-        rm {$i}_MEGArtReject-BadChanBaleenLP
-        rm {$i}_MEGArtReject-BadChanMaskedMM
+     echo $i
+     rm {$i}_MEGArtRejectATLLoc
+     rm {$i}_MEGArtRejectBaleenHP
+     rm {$i}_MEGArtRejectBaleenLP
+     rm {$i}_MEGArtRejectMaskedMM
+     rm {$i}_MEGArtRejectAXCPT
+     rm {$i}_MEGArtReject-BadChanATLLoc
+     rm {$i}_MEGArtReject-BadChanBaleenHP
+     rm {$i}_MEGArtReject-BadChanBaleenLP
+     rm {$i}_MEGArtReject-BadChanMaskedMM
+     rm {$i}_MEGArtReject-BadChanAXCPT
    end 
- 
+   cd ../../..
+
 echo 'CountEvents-CountBadChannels'
 cd /autofs/cluster/kuperberg/SemPrMM/MEG/data/
 foreach par ('ATLLoc' 'BaleenHP' 'BaleenLP' 'MaskedMM')
    echo $par
    foreach i ( $1* )
- #     echo $i 
+      echo $i 
       cd /autofs/cluster/kuperberg/SemPrMM/MEG/data/$i/ave_projon/logs/
       if ($par == 'ATLLoc') then
           if ( -e /autofs/cluster/kuperberg/SemPrMM/MEG/data/$i/ave_projon/logs/{$i}_{$par}-ave.log) then
-              foreach t ( {$i}_{$par}*-ave.log ) 
-	             python /cluster/kuperberg/SemPrMM/MEG/scripts/countBadChan.py $t $par 
+              foreach t ( {$i}_ATLLoc-ave.log ) 
+                  python /cluster/kuperberg/SemPrMM/MEG/scripts/countBadChan.py $t $par 
               end
           endif
       else
          if (-e /autofs/cluster/kuperberg/SemPrMM/MEG/data/$i/ave_projon/logs/{$i}_ATLLoc-ave.log ) then
               foreach t ( {$i}_{$par}Run?-ave.log ) 
-	          python /cluster/kuperberg/SemPrMM/MEG/scripts/countBadChan.py $t $par
+                  python /cluster/kuperberg/SemPrMM/MEG/scripts/countBadChan.py $t $par
               end
          endif
       endif      
@@ -41,17 +45,37 @@ foreach par ('ATLLoc' 'BaleenHP' 'BaleenLP' 'MaskedMM')
     end
 end
 
+
+
+if ($1 == 'ya') then
+    echo 'AXCPT for ya'
+    foreach i ($1*)
+      cd /autofs/cluster/kuperberg/SemPrMM/MEG/data/$i/ave_projon/logs
+      if (-e /autofs/cluster/kuperberg/SemPrMM/MEG/data/$i/ave_projon/logs/{$i}_AXCPTRun1-ave.log) then 
+          foreach t ( {$i}_AXCPTRun?-ave.log ) 
+                  rm {$i}_MEGArtReject{$t}
+                  rm {$i}_MEGArtReject-BadChan{$t}
+                 # python /cluster/kuperberg/SemPrMM/MEG/scripts/countBadChan.py $t AXCPT
+          end
+      endif
+      cd ../../..
+    end
+endif
+ 
+cd ../../..
+
 echo 'Clearing existing results from MEG/results folder'
 cd /autofs/cluster/kuperberg/SemPrMM/MEG/results/MEGArtRej
    rm $1_MEGArtRejSummaryATLLoc
    rm $1_MEGArtRejSummaryBaleenHP
    rm $1_MEGArtRejSummaryBaleenLP
    rm $1_MEGArtRejSummaryMaskedMM
+   rm $1_MEGArtRejSummaryAXCPT
 
 echo 'ComputeEvents'
 cd /autofs/cluster/kuperberg/SemPrMM/MEG/data/
-foreach par ( 'ATLLoc' 'BaleenHP' 'BaleenLP' 'MaskedMM' )
+foreach par ( 'AXCPT' 'ATLLoc' 'BaleenHP' 'BaleenLP' 'MaskedMM' )
    echo $par
-   python /cluster/kuperberg/SemPrMM/MEG/scripts/computeEvents.py $1 $par /autofs/cluster/kuperberg/SemPrMM/MEG/results/MEGArtRej/$1_MEGArtRejSummary-$par
+   python /cluster/kuperberg/SemPrMM/MEG/scripts/computeEvents.py $1 $par /cluster/kuperberg/SemPrMM/MEG/results/MEGArtRej/$1_MEGArtRejSummary-$par
 end
 echo 'Results saved in MEG/results/MEGArtRej folder' 
