fedf3e60d5e5306c2788cb8f591a8464bcf63566
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2012-02-10 12:21:59 -0500

    cleaned up commenting in countBadChan.py

--- countBadChan.py
+++ countBadChan.py
@@ -2,9 +2,10 @@ import sys
 import os 
 import readInput
 
-
+#This script is called by MEEGArtReject.sh
 #The MEEGArtReject_ file contains the total number of specified triggers encountered by MNE and the number rejected
-#The 
+#The MEEGArtReject-BadChan file contains a list of channels that caused rejection and the # of trials rejected
+#These are fed into MEEGArtReject.sh
 
 def countBadChan(inFile, par):
         
@@ -28,7 +29,6 @@ def countBadChan(inFile, par):
     	badChanList=list(set(badTrials)) #finds the items in a list(first occurances)
     	
     	##Create critical data table of bad channels and the number of trials they caused rejection
-    	##Create critical data table of bad channels and the number of trials they caused rejection
     	for chan in badChanList:
     		badchans.append([chan, badTrials.count((chan))])
                                     
@@ -48,11 +48,4 @@ def countBadChan(inFile, par):
         for row in badchans:
             myFile3.write("\n")
             myFile3.write(row[0])
-            myFile3.write("\t")
-            myFile3.write(str(row[1]))
-            myFile3.write("\n")
-        myFile3.close()
-
-if __name__ == "__main__":
-   
-   countBadChan(sys.argv[1], sys.argv[2])
+            myFil
