56cc6315a1f0be366772a9f398974b18b0fee77b
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2012-02-10 11:24:34 -0500

    added labels to source_roiWin2R_Baleen.py

--- source_roiWin2R_Baleen.py
+++ source_roiWin2R_Baleen.py
@@ -43,7 +43,7 @@ data_path = '/cluster/kuperberg/SemPrMM/MEG/results/source_space/ga_stc'
 protocolList = ['BaleenLP_All', 'BaleenLP_All', 'BaleenHP_All', 'BaleenHP_All']
 condList = ['1', '2', '1', '2']
 hemList = ['lh','rh']
-labelList = ['G_front_inf-Opercular', 'G_front_inf-Triangul','G_front_inf-Orbital', 'Pole_temporal', 'G_temp_ant-sup-Lateral','G_temp_post-sup-Lateral','S_temporal_ant-sup','S_temporal_post-sup','G_temporal_ant-middle','G_temporal_post-middle','G_temporal_inf','S_temporal_inf','G_pariet_inf-Angular','aparc2009-aMTGSTS','aparc2009-pMTGSTS','aparc2009-IFG']
+labelList = ['G_front_inf-Opercular', 'G_front_inf-Triangul','G_front_inf-Orbital', 'Pole_temporal', 'G_temp_ant-sup-Lateral','G_temp_post-sup-Lateral','S_temporal_ant-sup','S_temporal_post-sup','G_temporal_ant-middle','G_temporal_post-middle','G_temporal_inf','S_temporal_inf','G_pariet_inf-Angular','G_temp_sup-Plan_polar','aparc2009-aMTGSTS','aparc2009-pMTGSTS','aparc2009-IFG']
 
 ##Convert input times from ms to samples
 baseline=100 #ms
