ca48b3f141d18bebb874b05bbad44ff289c08c72
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2012-02-29 17:10:39 -0500

    Updated script to compute the mean accuracies and write another output text file

--- behav_save_df_meg.R
+++ behav_save_df_meg.R
@@ -5,6 +5,48 @@ fileName <- paste(filePath,'MEG_',subjType,'_',exp,"_accuracy.log",sep="")
 buffer<-read.table(fileName)
 behavData.all<-data.frame(subj=factor(buffer$V1),cond=factor(buffer$V2),acc=buffer$V3,rt=buffer$V4)
 
-outFile <- paste(filePath,'MEG_',subjType,'_',exp,".df",sep="")
-save(behavData.all,file=outFile)
+
+
+LPprime<-behavData.all[behavData.all$cond == "LP_AniPrime", ]
+LPtarg <-behavData.all[behavData.all$cond == "LP_AniTarg", ]
+HPprime<-behavData.all[behavData.all$cond == "HP_AniPrime", ]
+HPtarg <-behavData.all[behavData.all$cond == "HP_AniTarg", ]
+
+LPprimeMean<-mean(LPprime$acc)
+LPtargMean<-mean(LPtarg$acc)
+HPprimeMean<-mean(HPprime$acc)
+HPtargMean<-mean(HPtarg$acc)
+
+LPprimetarg<-behavData.all[behavData.all$cond == "LP_AniPrime" | behavData.all$cond == "LP_AniTarg", ]
+HPprimetarg<-behavData.all[behavData.all$cond == "HP_AniPrime" | behavData.all$cond == "HP_AniTarg", ]
+
+LPprimetargMean<-mean(LPprimetarg$acc)
+HPprimetargMean<-mean(HPprimetarg$acc)
+
+LPHPprimetarg<-behavData.all[behavData.all$cond == "LP_AniPrime" | behavData.all$cond == "LP_AniTarg" | behavData.all$cond == "HP_AniPrime" | behavData.all$cond == "HP_AniTarg", ]
+
+LPHPprimetargMean<-mean(LPHPprimetarg$acc)
+
+outFile2 <- paste(filePath,'MEG_',subjType,'_',exp,".txt",sep="")
+sink(outFile2)
+print("Table of Mean Accuracies")
+print("LPprimeMean")
+print(LPprimeMean)
+print("HPprimeMean")
+print(HPprimeMean)
+print("LPtargMean")
+print(LPtargMean)
+print("HPtargMean")
+print(HPtargMean)
+
+print("Table of Mean Accuracies for LP and HP")
+print("LPprimetargMean")
+print(LPprimetargMean)
+print("HPprimetargMean")
+print(HPprimetargMean)
+
+print("LPHPprimetargMean")
+print(LPHPprimetargMean)
+
+sink()
 }
