cb41901b2e385614d3e6ac0f89f005cf88d094c2
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2012-03-01 20:18:15 -0500

    cut the behav_save script down to simply saving the df

--- behav_save_df_meg.R
+++ behav_save_df_meg.R
@@ -2,51 +2,10 @@ behav_save_df_meg <-function(subjType,exp){
 	
 filePath <- "/cluster/kuperberg/SemPrMM/MEG/results/behavioral_accuracy/R/"
 fileName <- paste(filePath,'MEG_',subjType,'_',exp,"_accuracy.log",sep="")
-buffer<-read.table(fileName)
+buffer<-read.table(skip=1, fileName, header=FALSE)
 behavData.all<-data.frame(subj=factor(buffer$V1),cond=factor(buffer$V2),acc=buffer$V3,rt=buffer$V4)
 
+outFile <-paste(filePath, 'MEG_', subjType, '_', exp, '_accuracy.df', sep="")
+save(behavData.all, file=outFile)
 
-
-LPprime<-behavData.all[behavData.all$cond == "LP_AniPrime", ]
-LPtarg <-behavData.all[behavData.all$cond == "LP_AniTarg", ]
-HPprime<-behavData.all[behavData.all$cond == "HP_AniPrime", ]
-HPtarg <-behavData.all[behavData.all$cond == "HP_AniTarg", ]
-
-LPprimeMean<-mean(LPprime$acc)
-LPtargMean<-mean(LPtarg$acc)
-HPprimeMean<-mean(HPprime$acc)
-HPtargMean<-mean(HPtarg$acc)
-
-LPprimetarg<-behavData.all[behavData.all$cond == "LP_AniPrime" | behavData.all$cond == "LP_AniTarg", ]
-HPprimetarg<-behavData.all[behavData.all$cond == "HP_AniPrime" | behavData.all$cond == "HP_AniTarg", ]
-
-LPprimetargMean<-mean(LPprimetarg$acc)
-HPprimetargMean<-mean(HPprimetarg$acc)
-
-LPHPprimetarg<-behavData.all[behavData.all$cond == "LP_AniPrime" | behavData.all$cond == "LP_AniTarg" | behavData.all$cond == "HP_AniPrime" | behavData.all$cond == "HP_AniTarg", ]
-
-LPHPprimetargMean<-mean(LPHPprimetarg$acc)
-
-outFile2 <- paste(filePath,'MEG_',subjType,'_',exp,".txt",sep="")
-sink(outFile2)
-print("Table of Mean Accuracies")
-print("LPprimeMean")
-print(LPprimeMean)
-print("HPprimeMean")
-print(HPprimeMean)
-print("LPtargMean")
-print(LPtargMean)
-print("HPtargMean")
-print(HPtargMean)
-
-print("Table of Mean Accuracies for LP and HP")
-print("LPprimetargMean")
-print(LPprimetargMean)
-print("HPprimetargMean")
-print(HPprimetargMean)
-
-print("LPHPprimetargMean")
-print(LPHPprimetargMean)
-
-sink()
 }
