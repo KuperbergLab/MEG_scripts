3b29b030211cb5bb0e44975120e9ceb875707421
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2012-03-02 09:50:17 -0500

    use aov to compute marginal means in behav_stats_meg and add type III SS ANOVA

--- behav_stats_meg.R
+++ behav_stats_meg.R
@@ -1,48 +1,49 @@
 behav_stats_meg <-function(subjType,exp){
+
+###This function outputs behavioral stats for the MEG session
+###Currently focuses on the animal (go) trials only
 	
 filePath <- "/cluster/kuperberg/SemPrMM/MEG/results/behavioral_accuracy/R/"
 fileName <- paste(filePath,'MEG_',subjType,'_',exp,"_accuracy.df",sep="")
 load(fileName)
 
-behavData.all = subset(behavData.all, subj != 'ya1')
-
-LPprime<-behavData.all[behavData.all$cond == "LP_AniPrime", ]
-LPtarg <-behavData.all[behavData.all$cond == "LP_AniTarg", ]
-HPprime<-behavData.all[behavData.all$cond == "HP_AniPrime", ]
-HPtarg <-behavData.all[behavData.all$cond == "HP_AniTarg", ]
-
-LPprimeMean<-mean(LPprime$acc)
-LPtargMean<-mean(LPtarg$acc)
-HPprimeMean<-mean(HPprime$acc)
-HPtargMean<-mean(HPtarg$acc)
-
-LPprimetarg<-behavData.all[behavData.all$cond == "LP_AniPrime" | behavData.all$cond == "LP_AniTarg", ]
-HPprimetarg<-behavData.all[behavData.all$cond == "HP_AniPrime" | behavData.all$cond == "HP_AniTarg", ]
-
-LPprimetargMean<-mean(LPprimetarg$acc)
-HPprimetargMean<-mean(HPprimetarg$acc)
-
-LPHPprimetarg<-behavData.all[behavData.all$cond == "LP_AniPrime" | behavData.all$cond == "LP_AniTarg" | behavData.all$cond == "HP_AniPrime" | behavData.all$cond == "HP_AniTarg", ]
-
-LPHPprimetargMean<-mean(LPHPprimetarg$acc)
-LPHPprimetargSD<-sd(LPHPprimetarg$acc)
- 
-
-numSubj <-nlevels(factor(behavData.all$subj, exclude= NA))
-
 outFile <- paste(filePath,'MEG_',subjType,'_',exp,"_acc_stats.txt",sep="")
 sink(outFile)
+
+##exclude subjects for whom there were errors in behavioral data recording
+behavData.all = subset(behavData.all, subj != 'ya1')
+
+##include only animal trials
+behavData.animal<-behavData.all[behavData.all$cond == "LP_AniPrime" | behavData.all$cond == "LP_AniTarg" | behavData.all$cond == "HP_AniPrime" | behavData.all$cond == "HP_AniTarg", ]
+
+###################################################
+#################DESCRIPTIVE STATS#################
+
+##Factorize data and print descriptive means
+behavData.animal$prop<-factor(behavData.animal$cond,exclude=NULL);
+levels(behavData.animal$prop)<-c("hi","hi","lo","lo");
+behavData.animal$pos <-factor(behavData.animal$cond, exclude=NULL);
+levels(behavData.animal$pos) <-c("prime","targ","prime","targ")
+
+behavData.animal.aov = aov(acc ~ prop * pos + Error(subj/(prop * pos)),data=behavData.animal)
+
+numSubj <-nlevels(factor(behavData.all$subj, exclude= NA))
 print(paste("Table of Mean Accuracies, n:", numSubj,sep=" "))
-print(c("LPprimeMean",round(LPprimeMean,3)))
-print(c("HPprimeMean",round(HPprimeMean,3)))
-print(c("LPtargMean",round(LPtargMean,3)))
-print(c("HPtargMean",round(HPtargMean,3)))
+print(model.tables(behavData.animal.aov, "means"), digits = 5)
 
-print("Table of Mean Accuracies for LP and HP")
-print(c("LPprimetargMean",round(LPprimetargMean,3)))
-print(c("HPprimetargMean",round(HPprimetargMean,4)))
+##compute overall SD
+LPHPprimetarg<-behavData.all[behavData.all$cond == "LP_AniPrime" | behavData.all$cond == "LP_AniTarg" | behavData.all$cond == "HP_AniPrime" | behavData.all$cond == "HP_AniTarg", ]
+LPHPprimetargSD<-sd(LPHPprimetarg$acc)
+print(paste("LPHPprimetargSD:",round(LPHPprimetargSD,3),sep=" "))
 
-print(paste("LPHPprimetargMean:",round(LPHPprimetargMean,3),"   SD:",round(LPHPprimetargSD,3),sep=" "))
+##################################################
+##################ANOVAS##########################
+
+library('ez')
+eztest <-ezANOVA(data=behavData.animal,dv = .(acc),wid=.(subj),within=.(prop, pos),type=3,detailed=TRUE)
+print(eztest)
+
+
 
 sink()
 }
