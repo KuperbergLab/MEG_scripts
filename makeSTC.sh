d6be0923044799f13030e6a5bcc651e93eac7045
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2012-03-12 13:20:06 -0400

    changed parameters in makeSTC.sh

--- makeSTC.sh
+++ makeSTC.sh
@@ -3,10 +3,10 @@
 #usage makeSTC.sh subjID logfile
 #you need to change the exp variable and condList below to run STCs for your experiment of interest
 
-set exp = "MaskedMM_All"
+set exp = "BaleenHP_All"
 echo $exp
 
-set condList = ( 1 2 3 4 )
+set condList = ( 3 4 )
 echo $condList
 
 ####################################
@@ -40,23 +40,26 @@ mkdir stc/$exp
 
 foreach c ($condList)
 
-	echo Unmorphed mne >>& $log
-	mne_make_movie --inv $1_$exp-ave-7-meg-inv.fif --meas $1_{$exp}-ave.fif --set $c --bmin -100 --bmax -.01 --stc stc/$exp/$1_{$exp}_c{$c}-mne.stc --smooth 7 >>& $log
-	
-	echo Unmorphed spm >>& $log
-	mne_make_movie --inv $1_$exp-ave-7-meg-inv.fif --meas $1_{$exp}-ave.fif --set $c --bmin -100 --bmax -.01 --stc stc/$exp/$1_{$exp}_c{$c}-spm.stc --smooth 7 --spm >>& $log
-
-	echo Unmorphed sLORETA >>& $log
-	mne_make_movie --inv $1_$exp-ave-7-meg-inv.fif --meas $1_{$exp}-ave.fif --set $c --bmin -100 --bmax -.01 --stc stc/$exp/$1_{$exp}_c{$c}-sLORETA.stc --smooth 7 --sLORETA >>& $log
-
-	echo Morphed mne >>& $log
-	mne_make_movie --inv $1_$exp-ave-7-meg-inv.fif --meas $1_{$exp}-ave.fif --set $c --bmin -100 --bmax -.01 --stc stc/$exp/$1_{$exp}_c{$c}M-mne.stc --smooth 7 --morph fsaverage >>& $log
+# 	echo Unmorphed mne >>& $log
+# 	mne_make_movie --inv $1_$exp-ave-7-meg-inv.fif --meas $1_{$exp}-ave.fif --set $c --bmin -100 --bmax -.01 --stc stc/$exp/$1_{$exp}_c{$c}-mne.stc --smooth 7 >>& $log
+# 	
+# 	echo Unmorphed spm >>& $log
+# 	mne_make_movie --inv $1_$exp-ave-7-meg-inv.fif --meas $1_{$exp}-ave.fif --set $c --bmin -100 --bmax -.01 --stc stc/$exp/$1_{$exp}_c{$c}-spm.stc --smooth 7 --spm >>& $log
+# 
+# 	echo Unmorphed sLORETA >>& $log
+# 	mne_make_movie --inv $1_$exp-ave-7-meg-inv.fif --meas $1_{$exp}-ave.fif --set $c --bmin -100 --bmax -.01 --stc stc/$exp/$1_{$exp}_c{$c}-sLORETA.stc --smooth 7 --sLORETA >>& $log
+# 
+# 	echo Morphed mne >>& $log
+# 	mne_make_movie --inv $1_$exp-ave-7-meg-inv.fif --meas $1_{$exp}-ave.fif --set $c --bmin -100 --bmax -.01 --stc stc/$exp/$1_{$exp}_c{$c}M-mne.stc --smooth 7 --morph fsaverage >>& $log
 	
 	echo Morphed spm >>& $log
 	mne_make_movie --inv $1_$exp-ave-7-meg-inv.fif --meas $1_{$exp}-ave.fif --set $c --bmin -100 --bmax -.01 --stc stc/$exp/$1_{$exp}_c{$c}M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
 
-	echo Morphed spm >>& $log	
-	mne_make_movie --inv $1_$exp-ave-7-meg-inv.fif --meas $1_{$exp}-ave.fif --set $c --bmin -100 --bmax -.01 --stc stc/$exp/$1_{$exp}_c{$c}M-sLORETA.stc --smooth 7 --sLORETA --morph fsaverage >>& $log	
+# 	echo Morphed spm >>& $log	
+# 	mne_make_movie --inv $1_$exp-ave-7-meg-inv.fif --meas $1_{$exp}-ave.fif --set $c --bmin -100 --bmax -.01 --stc stc/$exp/$1_{$exp}_c{$c}M-sLORETA.stc --smooth 7 --sLORETA --morph fsaverage >>& $log	
+	
+# 	echo Morphed spm >> & $log
+# 	mne_make_movie --inv $1_$exp-ave-7-meg-eeg-inv.fif --meas $1_{$exp}-ave.fif --set $c --bmin -100 --bmax -.01 --stc stc/$exp/$1_{$exp}_c{$c}M-spm.stc --smooth 7 --spm --morph fsaverage >>& $log
 	
 end	
 
