8a3013a59861b15b73329e20781e3f2ac00e9db2
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2012-05-03 13:46:44 -0400

    call_ecgeogTable.sh: cShell script that calls the ecgeogTable.py recursively for all runs in each paradigm

---
+++ call_ecgeogTable.sh
@@ -0,0 +1,25 @@
+#!/bin/csh -f
+#usage: ./call_ecgeogTable.sh SubjID
+#eg: ./call_ecgeogTable.sh  ac1  
+
+
+
+cd /autofs/cluster/kuperberg/SemPrMM/MEG/data/$1
+if ( -e {$1}_ecgeog_eventsTable.txt ) then
+		rm {$1}_ecgeog_eventsTable.txt
+endif
+
+echo 'Run mne_compute_proj_ecg_eog.py for the subject'
+cd /autofs/cluster/kuperberg/SemPrMM/MEG/data/$1
+foreach par ('ATLLoc' 'BaleenHP' 'BaleenLP' 'MaskedMM')
+#foreach par ('MaskedMM')
+        if $par == 'ATLLoc' then
+              echo {$1}_ATLLoc_raw.fif
+		      python /cluster/kuperberg/SemPrMM/MEG/scripts/ecgeogTable.py {$1}_ATLLoc_raw.fif $par $1 {$1}_ecgeog_eventsTable.txt "ECG 063" 
+		else 
+		      foreach i ({$1}_{$par}Run?_raw.fif) 
+				echo $i			      
+				python /cluster/kuperberg/SemPrMM/MEG/scripts/ecgeogTable.py $i $par $1 {$1}_ecgeog_eventsTable.txt "ECG 063" 
+		      end
+		endif
+end
