7bf5ec7c9678deb7958c5f53e2b0eb18018fd467
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2012-05-09 09:36:10 -0400

    call_ecgeogTable.sh: Included column headings for the resulting table

--- call_ecgeogTable.sh
+++ call_ecgeogTable.sh
@@ -9,17 +9,21 @@ if ( -e {$1}_ecgeog_eventsTable.txt ) then
 		rm {$1}_ecgeog_eventsTable.txt
 endif
 
-echo 'Run mne_compute_proj_ecg_eog.py for the subject'
+echo 'Creating the ecgeogTable for the subject' $1
 cd /autofs/cluster/kuperberg/SemPrMM/MEG/data/$1
+
+echo "Paradigm	ECG_PulseRate EOG_Events" >> {$1}_ecgeog_eventsTable.txt
 foreach par ('ATLLoc' 'BaleenHP' 'BaleenLP' 'MaskedMM')
 #foreach par ('MaskedMM')
-        if $par == 'ATLLoc' then
-              echo {$1}_ATLLoc_raw.fif
-		      python /cluster/kuperberg/SemPrMM/MEG/scripts/ecgeogTable.py {$1}_ATLLoc_raw.fif $par $1 {$1}_ecgeog_eventsTable.txt "ECG 063" 
-		else 
-		      foreach i ({$1}_{$par}Run?_raw.fif) 
-				echo $i			      
-				python /cluster/kuperberg/SemPrMM/MEG/scripts/ecgeogTable.py $i $par $1 {$1}_ecgeog_eventsTable.txt "ECG 063" 
-		      end
-		endif
+	if $par == 'ATLLoc' then
+		  echo {$1}_ATLLoc_raw.fif
+		  
+		  python /cluster/kuperberg/SemPrMM/MEG/scripts/ecgeogTable.py {$1}_ATLLoc_raw.fif $par $1 {$1}_ecgeog_eventsTable.txt "ECG 063" 
+	else 
+		  foreach i ({$1}_{$par}Run?_raw.fif) 
+			echo $i			      
+			python /cluster/kuperberg/SemPrMM/MEG/scripts/ecgeogTable.py $i $par $1 {$1}_ecgeog_eventsTable.txt "ECG 063" 
+		  end
+	endif
 end
+chgrp lingua {$1}_ecgeog_eventsTable.txt
