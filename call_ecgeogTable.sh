3030a3bf441263c2c6f7f03a95038de3474ce3d5
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2012-05-10 16:15:30 -0400

    call_ecgeogTable.sh: minor changes

--- call_ecgeogTable.sh
+++ call_ecgeogTable.sh
@@ -12,9 +12,9 @@ endif
 echo 'Creating the ecgeogTable for the subject' $1
 cd /autofs/cluster/kuperberg/SemPrMM/MEG/data/$1
 
-echo "Paradigm	ECG_PulseRate EOG_Events" >> {$1}_ecgeog_eventsTable.txt
+echo "Paradigm	ECG_PulseRate EOG_PerMin" >> {$1}_ecgeog_eventsTable.txt
 foreach par ('ATLLoc' 'BaleenHP' 'BaleenLP' 'MaskedMM')
-#foreach par ('MaskedMM')
+#foreach par ('ATLLoc')
 	if $par == 'ATLLoc' then
 		  echo {$1}_ATLLoc_raw.fif
 		  python /cluster/kuperberg/SemPrMM/MEG/scripts/ecgeogTable.py {$1}_ATLLoc_raw.fif $par $1 {$1}_ecgeog_eventsTable.txt "ECG 063" 
