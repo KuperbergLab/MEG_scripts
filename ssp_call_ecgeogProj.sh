4cc144618017cf3ec024c1128f5275e6a815fc3d
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2012-05-25 18:27:00 -0400

    added command to make ssp directory to call_ecgeogProj.sh

---
+++ ssp_call_ecgeogProj.sh
@@ -0,0 +1,49 @@
+#!/bin/csh -f
+#usage: ./ssp_call_ecgeogProj.sh SubjID tag par
+#eg: ./ssp_call_ecgeogProj.sh ac1 ecg/eog/ecgeog ATLLoc
+
+# setenv USE_STABLE_5_0_0
+# source /usr/local/freesurfer/nmr-stable50-env
+# source /usr/pubsw/packages/mne/nightly/bin/mne_setup
+
+mkdir /autofs/cluster/kuperberg/SemPrMM/MEG/data/$1/ssp/
+
+if ( $#argv == 0 ) then 
+    echo "NO ARGUMENT SPECIFIED: Format - subjID tag paradigm"
+    exit 1
+endif
+
+echo 'Computing the Projections for the subject'
+cd /autofs/cluster/kuperberg/SemPrMM/MEG/data/$1/
+echo $3
+
+#Default Parameters
+set lfreq = 5
+set hfreq = 35
+set magrej = 4000
+set gradrej = 3000
+set eegrej = 500 
+if $2 == 'ecg' then 
+	set projtmin = -0.08
+	set projtmax = 0.08
+else if $2 == 'eog' then 
+	set projtmin = -0.15
+	set projtmax = 0.15
+# elif $2 == ecgeog 
+# 	set projtmin = -0.15
+# 	set projtmax = 0.15
+endif 
+
+  
+if $3 == 'ATLLoc' then
+         echo {$1}_ATLLoc_raw.fif
+         python  /cluster/kuperberg/SemPrMM/MEG/scripts/ssp_ecgeogProj.py  --in_path /cluster/kuperberg/SemPrMM/MEG/data/$1/ -i {$1}_ATLLoc_raw.fif -c "ECG 063" --tmin $projtmin --tmax $projtmax --l-freq $lfreq --h-freq $hfreq --rej-grad $gradrej --rej-mag $magrej --rej-eeg $eegrej --bad $1_bad_chan.txt --tag $2 --average
+else 
+	     foreach i ({$1}_{$3}Run?_raw.fif)
+		          python  /cluster/kuperberg/SemPrMM/MEG/scripts/ssp_ecgeogProj.py --in_path /cluster/kuperberg/SemPrMM/MEG/data/$1/ -i $i -c "ECG 063" --tmin $projtmin --tmax $projtmax --l-freq $lfreq --h-freq $hfreq --rej-grad $gradrej --rej-mag $magrej --rej-eeg $eegrej --average --bad $1_bad_chan.txt --tag $2		      			 
+	     end
+endif
+
+
+
+#   check if tag is ecg or eog or ecgeog then run the scripts accordingly/. compute_proj_ecg_eog - to be edited to suit this structure... 
