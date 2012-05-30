e4bb1754984341c5b419e64c228803c07361bd32
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2012-05-30 14:30:31 -0400

    preProc_cov.sh: edited to rename raw files to ssp_raw.fif

--- preProc_cov.sh
+++ preProc_cov.sh
@@ -27,22 +27,22 @@ python /cluster/kuperberg/SemPrMM/MEG/scripts/makeCovFiles.py $1  >>& $log
 echo "Computing covariance"  >>& $log
 date
 mne_process_raw \
- --raw ../$1_MaskedMMRun1_raw.fif \
- --raw ../$1_MaskedMMRun2_raw.fif \
+ --raw ../$1_MaskedMMRun1_ssp_raw.fif \
+ --raw ../$1_MaskedMMRun2_ssp_raw.fif \
  --cov ../cov/$1_MaskedMMRun1.cov \
  --cov ../cov/$1_MaskedMMRun2.cov \
  --gcov $1_MaskedMM_All-cov.fif \
  --projon --lowpass 20  >>& $log
  
 mne_process_raw \
- --raw ../$1_BaleenLPRun1_raw.fif \
- --raw ../$1_BaleenLPRun2_raw.fif \
- --raw ../$1_BaleenLPRun3_raw.fif \
- --raw ../$1_BaleenLPRun4_raw.fif \
- --raw ../$1_BaleenHPRun1_raw.fif \
- --raw ../$1_BaleenHPRun2_raw.fif \
- --raw ../$1_BaleenHPRun3_raw.fif \
- --raw ../$1_BaleenHPRun4_raw.fif \
+ --raw ../$1_BaleenLPRun1_ssp_raw.fif \
+ --raw ../$1_BaleenLPRun2_ssp_raw.fif \
+ --raw ../$1_BaleenLPRun3_ssp_raw.fif \
+ --raw ../$1_BaleenLPRun4_ssp_raw.fif \
+ --raw ../$1_BaleenHPRun1_ssp_raw.fif \
+ --raw ../$1_BaleenHPRun2_ssp_raw.fif \
+ --raw ../$1_BaleenHPRun3_ssp_raw.fif \
+ --raw ../$1_BaleenHPRun4_ssp_raw.fif \
  --cov ../cov/$1_BaleenLPRun1.cov \
  --cov ../cov/$1_BaleenLPRun2.cov \
  --cov ../cov/$1_BaleenLPRun3.cov \
@@ -58,15 +58,15 @@ mne_process_raw \
 if ( -e ../$1_AXCPTRun1_raw.fif ) then
 	if ( -e ../$1_AXCPTRun2_raw.fif ) then
 		mne_process_raw \
-		--raw ../$1_AXCPTRun1_raw.fif \
-		--raw ../$1_AXCPTRun2_raw.fif \
+		--raw ../$1_AXCPTRun1_ssp_raw.fif \
+		--raw ../$1_AXCPTRun2_ssp_raw.fif \
 		--cov ../cov/$1_AXCPTRun1.cov \
 		--cov ../cov/$1_AXCPTRun2.cov \
 		--gcov $1_AXCPT_All-cov.fif \
 		--projon --lowpass 20  >>& $log
 	else
 		mne_process_raw \
-		--raw ../$1_AXCPTRun1_raw.fif \
+		--raw ../$1_AXCPTRun1_ssp_raw.fif \
 		--cov ../cov/$1_AXCPTRun1.cov \
 		--gcov $1_AXCPT_All-cov.fif \
 		--projon --lowpass 20  >>& $log		
@@ -77,13 +77,13 @@ endif
 if ( $1 == 'ac19' ) then
 		
 	mne_process_raw \
-		--raw ../$1_BaleenLPRun1_raw.fif \
-		--raw ../$1_BaleenLPRun2_raw.fif \
-		--raw ../$1_BaleenLPRun4_raw.fif \
-		--raw ../$1_BaleenHPRun1_raw.fif \
-        --raw ../$1_BaleenHPRun2_raw.fif \
-        --raw ../$1_BaleenHPRun3_raw.fif \
-        --raw ../$1_BaleenHPRun4_raw.fif \
+		--raw ../$1_BaleenLPRun1_ssp_raw.fif \
+		--raw ../$1_BaleenLPRun2_ssp_raw.fif \
+		--raw ../$1_BaleenLPRun4_ssp_raw.fif \
+		--raw ../$1_BaleenHPRun1_ssp_raw.fif \
+        --raw ../$1_BaleenHPRun2_ssp_raw.fif \
+        --raw ../$1_BaleenHPRun3_ssp_raw.fif \
+        --raw ../$1_BaleenHPRun4_ssp_raw.fif \
 		--cov ../cov/$1_BaleenLPRun1.cov \
 		--cov ../cov/$1_BaleenLPRun2.cov \
 		--cov ../cov/$1_BaleenLPRun4.cov \
