e408b2482b87fbb3b1eaf4fc1be58a6a7e3abb20
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2012-06-13 12:33:11 -0400

    ssp_call_ecgeogProj.sh: Including both SSP vectors for EOG Projection in mag/grad/eeg and only 1 SSP vector in ECG Projection in mag/grad and 0 SSP vector in ECG Projection in eeg(since there is minimal/no ECG projection effect on teh eeg channnels).

--- ssp_call_ecgeogProj.sh
+++ ssp_call_ecgeogProj.sh
@@ -22,13 +22,20 @@ set lfreq = 5
 set hfreq = 35
 set magrej = 4000
 set gradrej = 3000
-set eegrej = 500 
+set eegrej = 500
 if $2 == 'ecg' then 
 	set projtmin = -0.08
 	set projtmax = 0.08
+    set ngrad = 1
+    set nmag = 1
+    set neeg = 0
 else if $2 == 'eog' then 
 	set projtmin = -0.15
 	set projtmax = 0.15
+    set ngrad = 2
+    set nmag = 2
+    set neeg = 2
+
 # elif $2 == ecgeog 
 # 	set projtmin = -0.15
 # 	set projtmax = 0.15
@@ -37,11 +44,11 @@ endif
   
 if $3 == 'ATLLoc' then
          echo {$1}_ATLLoc_raw.fif
-         python  /cluster/kuperberg/SemPrMM/MEG/scripts/ssp_ecgeogProj.py  --in_path /cluster/kuperberg/SemPrMM/MEG/data/$1/ -i {$1}_ATLLoc_raw.fif -c "ECG 063" --tmin $projtmin --tmax $projtmax --l-freq $lfreq --h-freq $hfreq --rej-grad $gradrej --rej-mag $magrej --rej-eeg $eegrej --bad $1_bad_chan.txt --tag $2 --average
+         python  /cluster/kuperberg/SemPrMM/MEG/scripts/ssp_ecgeogProj.py  --in_path /cluster/kuperberg/SemPrMM/MEG/data/$1/ -i {$1}_ATLLoc_raw.fif -c "ECG 063" --tmin $projtmin --tmax $projtmax --l-freq $lfreq --h-freq $hfreq --rej-grad $gradrej --rej-mag $magrej --rej-eeg $eegrej --bad $1_bad_chan.txt --tag $2 --average -g $ngrad -m $nmag -e $neeg
 else 
-	     foreach i ({$1}_{$3}Run?_raw.fif)
-		          python  /cluster/kuperberg/SemPrMM/MEG/scripts/ssp_ecgeogProj.py --in_path /cluster/kuperberg/SemPrMM/MEG/data/$1/ -i $i -c "ECG 063" --tmin $projtmin --tmax $projtmax --l-freq $lfreq --h-freq $hfreq --rej-grad $gradrej --rej-mag $magrej --rej-eeg $eegrej --average --bad $1_bad_chan.txt --tag $2		      			 
-	     end
+	      foreach i ({$1}_{$3}Run?_raw.fif) 
+		           python  /cluster/kuperberg/SemPrMM/MEG/scripts/ssp_ecgeogProj.py --in_path /cluster/kuperberg/SemPrMM/MEG/data/$1/ -i $i -c "ECG 063" --tmin $projtmin --tmax $projtmax --l-freq $lfreq --h-freq $hfreq --rej-grad $gradrej --rej-mag $magrej --rej-eeg $eegrej --average --bad $1_bad_chan.txt --tag $2 -g $ngrad -m $nmag -e $neeg
+          end
 endif
 
 
