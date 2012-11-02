7fa24f722b12e174987d9d1ddd92aa44acf772d7
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2012-11-02 12:08:26 -0400

    ssp_find_eog_event_cu.m: Creates an eog-eve.fif file after identifying the eog events defined by the parameters in the scriot and saves it in the ssp folder.

---
+++ ssp_find_eog_event_cu.m
@@ -0,0 +1,53 @@
+% function ssp_find_eog_event_cu(infif, inpath)
+
+%input file
+%in_fif_File = infif;
+%EOG Event file
+%[~, name, ~] = fileparts(infif);
+% [name1, remain]= strtok(name, '_');
+% [name2, ~]=strtok(remain, '_');
+% eog_eventFileName = [inpath, name1,'_', name2, '_eog-eve.fif'];
+
+in_fif_File='/autofs/cluster/kuperberg/SemPrMM/MEG/data/sc19/sc19_BaleenLPRun1_raw.fif';
+eog_eventFileName='/autofs/cluster/kuperberg/SemPrMM/MEG/data/sc19/ssp/sc19_BaleenLPRun1_eog1-eve.fif';
+%eog_figfile='/autofs/cluster/kuperberg/SemPrMM/MEG/data/sc1/ssp/sc1_BaleenHPRun2_m2sd_eog.png';
+
+%reading eog channels from data files
+[fiffsetup] = fiff_setup_read_raw(in_fif_File);
+channelNames = fiffsetup.info.ch_names;
+ch_EOG = strmatch('EOG',channelNames);
+sampRate = fiffsetup.info.sfreq;
+start_samp = fiffsetup.first_samp;
+end_samp = fiffsetup.last_samp;
+[eog] = fiff_read_raw_segment(fiffsetup, start_samp ,end_samp, ch_EOG(2));
+
+
+
+% Detecting Blinks
+filteog = eegfilt(eog, sampRate,0,10);
+EOG_type = 998;
+firstSamp = fiffsetup.first_samp;
+temp = filteog-mean(filteog);
+
+eog_std_dev_value=1;
+
+if sum(temp>(mean(temp)+2*std(temp))) > sum(temp<(mean(temp)+2*std(temp)))
+    
+    eog_events = peakfinder((filteog),eog_std_dev_value*std(filteog),-1);
+
+else
+    eog_events = peakfinder((filteog),eog_std_dev_value*std(filteog),1);
+
+end
+
+%  t=1:length(filteog);
+%  plot(t,filteog);
+%  hold on
+%  grid on
+%  axis tight
+%  plot(t(eog_events),filteog(eog_events),'r+')
+%  print( gcf, '-dpng', eog_figfile )
+
+writeEventFile(eog_eventFileName, firstSamp, eog_events, EOG_type);
+
+%end
