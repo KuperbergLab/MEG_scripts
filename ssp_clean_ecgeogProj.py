c132ad46aa82ff96651da1f443a169b8a8a85370
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2012-12-11 17:41:24 -0500

    ssp_clean_ecgeogProj.py:no change

--- ssp_clean_ecgeogProj.py
+++ ssp_clean_ecgeogProj.py
@@ -16,6 +16,7 @@ def compute_proj_ecg(in_path, in_fif_fname, tmin, tmax, n_grad, n_mag, n_eeg, l_
     # Reading fif File
     raw_in = fiff.Raw(in_fif_fname)
     prefix = in_fif_fname[:-8]
+ #   prefix = 'sc3_BaleenLPRun4'
     print prefix
     in_fif_fname = in_path + in_fif_fname
     print in_fif_fname
@@ -26,27 +27,27 @@ def compute_proj_ecg(in_path, in_fif_fname, tmin, tmax, n_grad, n_mag, n_eeg, l_
     ecg_event_fname = in_path + 'ssp/' + prefix + '_ecg-eve.fif'        
     flag = 0
     
-    print 'Implementing ECG artifact rejection on data'
-    ecg_events, _, _ = mne.artifacts.find_ecg_events(raw_in)
-    if not len(ecg_events) < 30:
-		print ecg_event_fname
-		print "Writing ECG events in %s" % ecg_event_fname
-		mne.write_events(ecg_event_fname, ecg_events)
+#     print 'Implementing ECG artifact rejection on data'
+#     ecg_events, _, _ = mne.artifacts.find_ecg_events(raw_in)
+#     if not len(ecg_events) < 30:
+#             print ecg_event_fname
+#             print "Writing ECG events in %s" % ecg_event_fname
+#             mne.write_events(ecg_event_fname, ecg_events)
 	
-		# Making projector
-		print 'Computing ECG projector'
-                print out_path
-	        #os.chdir('/cluster/kuperberg/SemPrMM/MEG/data/ac1/ssp')
-                #os.getcwd()
-		command = ('mne_process_raw --cd %s --raw %s --events %s --makeproj '
-				   '--projtmin %s --projtmax %s --saveprojtag _ecg_proj '
-				   '--projnmag %s --projngrad %s --projneeg %s --projevent 999 --highpass 5 '
-				   '--lowpass 35 --projmagrej 4000 --projgradrej 3000 --projeegrej 250 '
-		         	   % (in_path, in_fif_fname, ecg_event_fname, tmin, tmax, n_mag, n_grad, n_eeg)) ##10/1/12 CU after changing the number of projectors for ECG(mag1, grad1, eeg0) 
-		
-		st = os.system(command)
-		if st != 0:
-			print "Error while running : %s" % command
+    # Making projector
+    print 'Computing ECG projector'
+    print out_path
+    #os.chdir('/cluster/kuperberg/SemPrMM/MEG/data/ac1/ssp')
+    #os.getcwd()
+    command = ('mne_process_raw --cd %s --raw %s --events %s --makeproj '
+                       '--projtmin %s --projtmax %s --saveprojtag _ecg_proj '
+                       '--projnmag %s --projngrad %s --projneeg %s --projevent 999 --highpass 5 '
+                       '--lowpass 35 --projmagrej 4000 --projgradrej 3000 --projeegrej 250 '
+                       % (in_path, in_fif_fname, ecg_event_fname, tmin, tmax, n_mag, n_grad, n_eeg)) ##10/1/12 CU after changing the number of projectors for ECG(mag1, grad1, eeg0) 
+    
+    st = os.system(command)
+    if st != 0:
+            print "Error while running : %s" % command
                    
     return in_fif_fname, ecg_proj_fname, len(ecg_event_fname), out_fif_fname
 
