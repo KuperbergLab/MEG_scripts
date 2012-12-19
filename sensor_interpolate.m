3f4739ec804de75531f4170458a36670ea77abce
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2012-12-19 15:15:21 -0500

    sensor_interpolate.m: testing sc19 data structure

--- sensor_interpolate.m
+++ sensor_interpolate.m
@@ -7,23 +7,31 @@ function sensor_interpolate(exp,subjGroup,listPrefix)
 dataPath = '/autofs/cluster/kuperberg/SemPrMM/MEG/';
 disp(dataPath);
 subjList = (dlmread(strcat(dataPath,'scripts/function_inputs/',listPrefix, '.txt')))';
+newsubjList = {'ac31', 'sc19'};
 
 numChan = 70;
 disp(dataPath);
 for s = subjList
 
     fileName = strcat(dataPath,'data/', subjGroup,int2str(s),'/ave_projoff/',subjGroup,int2str(s),'_',exp,'-ave.fif');
+    %%fileName = strcat(dataPath,'data/', subjGroup,int2str(s),'/',subjGroup,int2str(s),'_',exp,'-ave.fif');
     subjStr = fiff_read_evoked_all(fileName); %%this holds the original data
     iStr = subjStr; %%this will hold the interpolated data
-    
  
     %% Read channel info
 
     allChans = [316:375 379:388];
- 
+    disp(s)
     %%fix subjects with different number of channels recorded
     if (s == 1 || s == 2 || s == 3 || s == 4) && strcmp(subjGroup,'ya')
         allChans = [316:375 380:389];
+    end
+    
+%     subjID = strcat(subjGroup, s);
+%     disp(subjID);
+    
+    if (s == 19) && strcmp(subjGroup,'sc') 
+        allChans = [307:366 370:379];
     end
       
     allX = [];
@@ -35,7 +43,7 @@ for s = subjList
     goodChanIndex = [];
     badChanList = [];
 
-    for i = 1:70
+    for i = 1:35
         allX(end+1) = subjStr.info.chs(allChans(i)).eeg_loc(1);
         allY(end+1) = subjStr.info.chs(allChans(i)).eeg_loc(2);
         allZ(end+1) = subjStr.info.chs(allChans(i)).eeg_loc(3);
@@ -45,17 +53,24 @@ for s = subjList
             goodX(end+1) = subjStr.info.chs(allChans(i)).eeg_loc(1);
             goodY(end+1) = subjStr.info.chs(allChans(i)).eeg_loc(2);
             goodZ(end+1) = subjStr.info.chs(allChans(i)).eeg_loc(3);
+            if (subjStr.info.chs(allChans(i)).eeg_loc(1) == 0) 
+                scatter3(goodX, goodY, goodZ); hold on;
+            else 
+                scatter3(goodX, goodY, goodZ, 'filled'); hold on;
+            end
             goodChanIndex(end+1) = i;
         else
             badChanList(end+1) = badTest; %%make a list of bad EEG chan
         end
     end
-
+    xlim([-0.3 0.3])
+    ylim([-0.3 0.3])
+    zlim([-0.3 0.3])
     goodChans = allChans(goodChanIndex);  %%goodChans is in MNE indices (316-389)
     
     goodPos = [goodX;goodY;goodZ];
     allPos = [allX;allY;allZ];
-
+    
     badChanList
     iStr.info.bads(badChanList) = [];  %unmark the bad EEG chan in the interp. structure
  
