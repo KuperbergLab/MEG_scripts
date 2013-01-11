837f3d0025e5aa4e3ff774912903223925b85665
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2013-01-11 14:16:21 -0500

    sensor_erpWin2R_Baleen_acsc.m: Updated EEG channel selection- after new structure change

--- sensor_erpWin2R_Baleen_acsc.m
+++ sensor_erpWin2R_Baleen_acsc.m
@@ -11,9 +11,9 @@ dataPath = '/autofs/cluster/kuperberg/SemPrMM/MEG/';
 subjList = (dlmread(strcat(dataPath,'scripts/function_inputs/',listPrefix, '.txt')))';
 numSubj = size(subjList,2);
 numChan = 70;
-chan = [316:375 379:388];
-
+chan = [307:366 370:379]; %Not including the STI channels and RMAST 1/11/13
 baselineV = 1:60;
+
 if strcmp(subjGroup,'ac')
 	groupNum = 1
 elseif strcmp(subjGroup,'sc')
