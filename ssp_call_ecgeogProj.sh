409cea2f6a5a739feaae07587b550f39a083740c
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2013-01-15 15:38:42 -0500

    ssp_call_ecgeogProj.sh: Modified the input arguments for script. Keeping ECG projection fixed(1 1 0) and supplying the EOG proj vectors in the input line along with subject ID, tag and Paradigm.

--- ssp_call_ecgeogProj.sh
+++ ssp_call_ecgeogProj.sh
@@ -1,6 +1,6 @@
 #!/bin/csh -f
-#usage: ./ssp_call_ecgeogProj.sh SubjID tag par clean
-#eg: ./ssp_call_ecgeogProj.sh ac1 ecg/eog/ecgeog ATLLoc clean
+#usage: ./ssp_call_ecgeogProj.sh SubjID tag par nmag ngrad neeg
+#eg: ./ssp_call_ecgeogProj.sh ac1 ecg/eog/ecgeog ATLLoc 1 1 0
 
 # setenv USE_STABLE_5_0_0
 # source /usr/local/freesurfer/nmr-stable50-env
@@ -17,12 +17,12 @@ echo 'Computing the Projections for the subject'
 cd /autofs/cluster/kuperberg/SemPrMM/MEG/data/$1/
 echo $3
 
-set ngrad = 1
-set nmag = 1
-set neeg = 1 
+# set ngrad = 1
+# set nmag = 1
+# set neeg = 1 
 ##10/1/12 Using new set of projectors for ECG(1mag, 1grad, 0eeg-specified in ssp_clean_ecgeogProj.py script) and EOG(1mag 1grad 1eeg).
 
-#Default Parameters
+##Default Parameters
 set magrej = 4000
 set gradrej = 3000
 set eegrej = 500
@@ -39,10 +39,9 @@ else if $2 == 'eog' then
     set hfreq = 0.3 
     set projtmin = -0.2
     set projtmax = 0.2
-    set ngrad = 1
-    set nmag = 1 
-    set neeg = 1 ##2nd ssp vector had no/negative effect so changed back to 1 ##changed back to 3 after modifying HPF 
-
+    set ngrad = 2 
+    set nmag = 2
+    set neeg = 2 ##2nd ssp vector had no/negative effect so changed back to 1 ##changed back to 3 after modifying HPF 
 else if $2 == 'ecgeog' then
     set lfreq = 35
     set hfreq = 0.3 
@@ -50,45 +49,18 @@ else if $2 == 'ecgeog' then
     set projtmax = 0.2
 endif 
 
-if $4 == 'clean' then
 
-			if $3 == 'ATLLoc' then
-				 echo {$1}_ATLLoc_raw.fif                                  
-				 python  /cluster/kuperberg/SemPrMM/MEG/scripts/ssp_clean_ecgeogProj.py  --in_path /cluster/kuperberg/SemPrMM/MEG/data/$1/ -i {$1}_ATLLoc_raw.fif -c "ECG 063" --tmin $projtmin --tmax $projtmax --l-freq $lfreq --h-freq $hfreq --rej-grad $gradrej --rej-mag $magrej --rej-eeg $eegrej --bad $1_bad_chan.txt --tag $2 -g $ngrad -m $nmag -e $neeg
-                                 mv /cluster/kuperberg/SemPrMM/MEG/data/$1/{$1}_ATLLoc_ecg_proj.fif /cluster/kuperberg/SemPrMM/MEG/data/$1/ssp/
-                                 mv /cluster/kuperberg/SemPrMM/MEG/data/$1/{$1}_ATLLoc_eog_proj.fif /cluster/kuperberg/SemPrMM/MEG/data/$1/ssp/
-			else 
-				  foreach i ({$1}_{$3}Run?_raw.fif)
-						   python  /cluster/kuperberg/SemPrMM/MEG/scripts/ssp_clean_ecgeogProj.py --in_path /cluster/kuperberg/SemPrMM/MEG/data/$1/ -i $i -c "ECG 063" --tmin $projtmin --tmax $projtmax --l-freq $lfreq --h-freq $hfreq --rej-grad $gradrej --rej-mag $magrej --rej-eeg $eegrej --tag $2 
-                                                                           
-				  end
-#  				  mv /cluster/kuperberg/SemPrMM/MEG/data/$1/ssp/{$1}_{$3}Run?_eog_proj.fif /cluster/kuperberg/SemPrMM/MEG/data/$1/
-#  				  mv /cluster/kuperberg/SemPrMM/MEG/data/$1/{$1}_{$3}Run?_ecg_proj.fif /cluster/kuperberg/SemPrMM/MEG/data/$1/ssp/
-#  				  mv /cluster/kuperberg/SemPrMM/MEG/data/$1/{$1}_{$3}Run?_ecgeog_proj.fif /cluster/kuperberg/SemPrMM/MEG/data/$1/ssp/
+if $3 == 'ATLLoc' then
+	 echo {$1}_ATLLoc_raw.fif                                  
+	 python  /cluster/kuperberg/SemPrMM/MEG/scripts/ssp_clean_ecgeogProj.py  --in_path /cluster/kuperberg/SemPrMM/MEG/data/$1/ -i {$1}_ATLLoc_raw.fif -c "ECG 063" --tmin $projtmin --tmax $projtmax --l-freq $lfreq --h-freq $hfreq --rej-grad $gradrej --rej-mag $magrej --rej-eeg $eegrej --bad $1_bad_chan.txt --tag $2 -g $5 -m $4 -e $6
+else 
+	  foreach i ({$1}_{$3}Run?_raw.fif)
+			   python  /cluster/kuperberg/SemPrMM/MEG/scripts/ssp_clean_ecgeogProj.py --in_path /cluster/kuperberg/SemPrMM/MEG/data/$1/ -i $i -c "ECG 063" --tmin $projtmin --tmax $projtmax --l-freq $lfreq --h-freq $hfreq --rej-grad $gradrej --rej-mag $magrej --rej-eeg $eegrej --tag $2 -g $5 -m $4 -e $6											   
+	  end
+endif    
 
-			endif
-
-    
-endif
-
-
-
-
-# if $3 == 'ATLLoc' then
-# 	 echo {$1}_ATLLoc_raw.fif                                  
-# 	 python  /cluster/kuperberg/SemPrMM/MEG/scripts/ssp_ecgeogProj.py  --in_path /cluster/kuperberg/SemPrMM/MEG/data/$1/ -i {$1}_ATLLoc_raw.fif -c "ECG 063" --tmin $projtmin --tmax $projtmax --l-freq $lfreq --h-freq $hfreq --rej-grad $gradrej --rej-mag $magrej --rej-eeg $eegrej --bad $1_bad_chan.txt --tag $2 -g $ngrad -m $nmag -e $neeg --bad $1_bad_chan.txt
-# 					 mv /cluster/kuperberg/SemPrMM/MEG/data/$1/{$1}_ATLLoc_ecg_proj.fif /cluster/kuperberg/SemPrMM/MEG/data/$1/ssp/
-# 					 mv /cluster/kuperberg/SemPrMM/MEG/data/$1/{$1}_ATLLoc_eog_proj.fif /cluster/kuperberg/SemPrMM/MEG/data/$1/ssp/
-# else 
-# 	  foreach i ({$1}_{$3}Run1_raw.fif) 
-# 			   python  /cluster/kuperberg/SemPrMM/MEG/scripts/ssp_ecgeogProj.py --in_path /cluster/kuperberg/SemPrMM/MEG/data/$1/ -i $i -c "ECG 063" --tmin $projtmin --tmax $projtmax --l-freq $lfreq --h-freq $hfreq --rej-grad $gradrej --rej-mag $magrej --rej-eeg $eegrej --tag $2 -g $ngrad -m $nmag -e $neeg --bad $1_bad_chan.txt
-# 															   
-# 	  end
-# 	  mv /cluster/kuperberg/SemPrMM/MEG/data/$1/ssp/{$1}_{$3}Run?_eog_proj.fif /cluster/kuperberg/SemPrMM/MEG/data/$1/
-# # 				  mv /cluster/kuperberg/SemPrMM/MEG/data/$1/{$1}_{$3}Run?_ecg_proj.fif /cluster/kuperberg/SemPrMM/MEG/data/$1/ssp/
-# # 				  mv /cluster/kuperberg/SemPrMM/MEG/data/$1/{$1}_{$3}Run?_ecgeog_proj.fif /cluster/kuperberg/SemPrMM/MEG/data/$1/ssp/
-# 
-# endif
-
-
-#   check if tag is ecg or eog or ecgeog then run the scripts accordingly/. compute_proj_ecg_eog - to be edited to suit this structure... 
+mv /cluster/kuperberg/SemPrMM/MEG/data/$1/{$1}_ATLLoc_ecg_proj.fif /cluster/kuperberg/SemPrMM/MEG/data/$1/ssp/
+mv /cluster/kuperberg/SemPrMM/MEG/data/$1/{$1}_ATLLoc_eog_proj.fif /cluster/kuperberg/SemPrMM/MEG/data/$1/ssp/
+mv /cluster/kuperberg/SemPrMM/MEG/data/$1/{$1}_{$3}Run?_eog_proj.fif /cluster/kuperberg/SemPrMM/MEG/data/$1/ssp/
+mv /cluster/kuperberg/SemPrMM/MEG/data/$1/{$1}_{$3}Run?_ecg_proj.fif /cluster/kuperberg/SemPrMM/MEG/data/$1/ssp/
+mv /cluster/kuperberg/SemPrMM/MEG/data/$1/{$1}_{$3}Run?_ecgeog_proj.fif /cluster/kuperberg/SemPrMM/MEG/data/$1/ssp/
