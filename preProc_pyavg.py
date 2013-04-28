053e81ecadeb8ba4592e63877c7993f635ddfe14
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2013-04-28 16:13:58 -0400

    preProc_pyavg.py script for doing averages in python

--- preProc_pyavg.py
+++ preProc_pyavg.py
@@ -1,4 +1,5 @@
 
+import condCodes as cc
 import mne
 from mne import fiff
 from mne.epochs import combine_event_ids
@@ -6,15 +7,35 @@ import numpy
 import argparse
 import copy
 
+
 parser = argparse.ArgumentParser(description='Get input')
 parser.add_argument('subj',type=str)
+parser.add_argument('exp',type=str)
 args=parser.parse_args()
 
 print args.subj
+print args.exp
+exp = args.exp
+
 ####Experiment specific parameters (needs to be softcoded)
 
-runs = ['Run1','Run2','Run3','Run4']
-exp = 'BaleenHP'
+runs = cc.runDict[exp]
+
+if args.subj == 'ac8' and exp == 'BaleenLP':
+    runs = ['Run1','Run3','Run4']
+if args.subj == 'ac19' and exp == 'BaleenLP':
+    runs = ['Run1','Run2','Run4']
+if args.subj == 'ac13' and exp == 'BaleenLP':
+    runs = ['Run1','Run2','Run4']
+
+
+
+
+if exp == 'BaleenHP':
+    event_id = dict(Related=6, Unrelated=7, RelatedFiller=8, UnrelatedFiller=9, ProbeTarget=10, ProbePrime=12, Prime=14, RelatedFillerExtra=18)
+if exp == 'BaleenLP':
+    event_id = dict(Related=1, Unrelated=2, UnrelatedFiller=4, ProbeTarget=5, ProbePrime=11)
+
 tmin = -.1
 tmax = .7
 
@@ -25,36 +46,36 @@ magFlat = 1e-14
 gradFlat = 1000e-15
 
 ## SUBJECT-SPECIFIC REJECTION THRESHOLD MODIFICATIONS
-if args.subj == "ya31" or args.subj == 'sc9':
-    magRej = "4000e-15"   ##note exception for ya31 and sc9, whose magnetometers were baseline noisy
+if args.subj == "ya31" or args.subj == "sc9":
+    magRej = 4000e-15   ##note exception for ya31 and sc9, whose magnetometers were baseline noisy
  	
 if args.subj == "ya5" or args.subj == "ya21" or args.subj == "ya18" or args.subj == "ya27" or args.subj == "ya31":
-    eegRej = "150e-6"   ##because of alpha for ya21
+    eegRej = 150e-6   ##because of alpha for ya21
  	
 if args.subj == "ya23":
-    eegRej = "125e-6"
+    eegRej = 125e-6
  
 if args.subj == "ya15":
-    eegRej = "80e-6"
+    eegRej = 80e-6
 				
 if args.subj == "ya26":
-    eegRej = "90e-6"
+    eegRej = 90e-6
 
 if args.subj == "ac2" or args.subj == "ac7" or args.subj == "sc19": 
-    eegRej = "1"
+    eegRej = 1
 		
 if args.subj == "sc17" or args.subj == "sc20":
-    eegRej = "250e-6"
+    eegRej = 250e-6
 
 evokedRuns = []
 for evRun in runs:
     ####Subject and run-specific parameters
-
+    print evRun
     data_path = '/cluster/kuperberg/SemPrMM/MEG/data/'+args.subj+'/'
     raw_fname = data_path + args.subj+'_'+ exp +evRun+'_raw.fif'
     raw_ssp_fname = data_path + args.subj+'_'+ exp +evRun+'_ssp_raw.fif'
     event_fname = data_path + 'eve/'+args.subj+'_'+exp+evRun+'Mod.eve'
-    event_id = dict(Related=6, Unrelated=7, RelatedFiller=8, UnrelatedFiller=9, ProbeTarget=10, ProbePrime=12, Prime=14, RelatedFillerExtra=18)
+        
     print event_fname
 
     #   Setup for reading the original raw data and events
@@ -62,10 +83,14 @@ for evRun in runs:
     events = mne.read_events(event_fname)
     raw_skip = raw.first_samp
 
-    #   Correct events for the fact that ssp data has the skip removed
-    for row in events:
-        row[0] = row[0]-raw_skip
+    #   Correct events for the fact that ssp data has the skip removed, except sc9 for some reason
+    if not (args.subj == "sc9") and not (args.subj == 'ac12') and not (args.subj == 'ac13'):
+        for row in events:
+            row[0] = row[0]-raw_skip
 
+    # Merge events
+    #events = mne.event.merge_events(events,[6,8],20,replace_events=False)
+    #events = mne.event.merge_events(events,[7,9],21,replace_events=False)
     #   Read SSP raw data
     raw_ssp = fiff.Raw(raw_ssp_fname,preload=True)
 
@@ -87,15 +112,17 @@ for evRun in runs:
     epochs = mne.Epochs(raw_ssp, events, event_id, tmin, tmax, picks=picks, baseline=(None, 0),add_eeg_ref=False, proj=True,reject=dict(eeg=eegRej,mag=magRej,grad=gradRej),flat=dict(mag=magFlat, grad=gradFlat))
 
     #Combine conditions
-    combine_event_ids(epochs, ['Related', 'RelatedFiller'],{'Related120':68},copy=False)
-    combine_event_ids(epochs, ['Unrelated','UnrelatedFiller'],{'Unrelated120':79},copy=False)
-
+    #combine_event_ids(epochs, ['Related', 'RelatedFiller'],{'Related120':68},copy=False)
+    #combine_event_ids(epochs, ['Unrelated','UnrelatedFiller'],{'Unrelated120':79},copy=False)
 
     evokeds = [epochs[cond].average(picks=picks) for cond in event_id]
+    if exp == 'BaleenHP':
+        evoked_Rel = evokeds[4]+evokeds[5]
+        evoked_Unrel = evokeds[6]+evokeds[7]
+        evokeds.append(evoked_Rel)
+        evokeds.append(evoked_Unrel)
     fiff.write_evoked(data_path + 'ave_projon/'+args.subj+'_'+exp+evRun+'-ave.fif',evokeds)
-    
     evokedRuns.append(evokeds)
-
 
 #Make grand-average
 runData = []
