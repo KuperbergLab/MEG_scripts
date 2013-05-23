2e6b0329d7d30cb10f31eed930eb2a52cd2d3cef
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2013-05-23 13:50:33 -0400

    call_preProc_Pyavg.sh: Updated to specify the subject type and paradigm

--- call_preProc_Pyavg.sh
+++ call_preProc_Pyavg.sh
@@ -1,13 +1,13 @@
 #!/bin/csh -f
-## Usage ./call_preProc_Pyavg.sh subjectType 
+## Usage ./call_preProc_Pyavg.sh subjectType paradigm
 
 
 cd /autofs/cluster/kuperberg/SemPrMM/MEG/data/
 #foreach par ('BaleenHP' 'BaleenLP' 'ATLLoc' 'MaskedMM')
-foreach par ('BaleenHP')
-   echo $par
-   foreach i (`cat /autofs/cluster/kuperberg/SemPrMM/MEG/scripts/function_inputs/$1.meg.all.txt`)   
+#foreach par ('BaleenLP' 'MaskedMM')
+echo $2
+foreach i (`cat /autofs/cluster/kuperberg/SemPrMM/MEG/scripts/function_inputs/$1.meg.all.txt`)   
          set subj = $1$i
          echo $subj
-         python  /cluster/kuperberg/SemPrMM/MEG/scripts/preProc_avg.py $subj $par
-   end
+         python  /cluster/kuperberg/SemPrMM/MEG/scripts/preProc_avg.py $subj $2
+end
