2a9bd4a32f8d054890ca93c97c5245556290ccf2
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2013-05-30 22:06:33 -0400

    changed buffer input for quad48mid for sensor_save_df_main.R

--- sensor_save_df_main.R
+++ sensor_save_df_main.R
@@ -3,7 +3,12 @@ sensor_save_df_main <-function(filePrefix,exp,chanGroupName,t1,t2){
 filePath <- "/cluster/kuperberg/SemPrMM/MEG/results/sensor_level/R/"
 fileName <- paste(filePath,filePrefix,".",exp,".",chanGroupName,".",t1,"-",t2,".txt",sep="")
 buffer<-read.table(fileName)
-erpData.all<-data.frame(subj=factor(buffer$V1),cond=factor(buffer$V2),elec=factor(buffer$V3),amp=buffer$V4,hemCode=factor(buffer$V5),antCode=factor(buffer$V6),midV=factor(buffer$V7),midH=factor(buffer$V8),elec9=factor(buffer$V9))
+print(buffer)
+
+####Specific code for each channel grouping
+if(chanGroupName == 'quad48Mid'){
+erpData.all<-data.frame(subj=factor(buffer$V6),subjGroup=factor(buffer$V5),exp=factor(buffer$V7),cond=factor(buffer$V8),elec=factor(buffer$V4),hemCode=factor(buffer$V1),antCode=factor(buffer$V2),chanLabel=factor(buffer$V3),amp=buffer$V9)
+}
 
 outFile <- paste(filePath,filePrefix,".",exp,".",chanGroupName,t1,"-",t2,".df",sep="")
 save(erpData.all,file=outFile)
