27bbd8f9be69b0bb3e0514bedd2078ab9789e002
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2013-05-30 21:29:33 -0400

    changed file input for sensor_save_df_main.R for changroup

--- sensor_save_df_main.R
+++ sensor_save_df_main.R
@@ -1,10 +1,10 @@
-sensor_save_df_main <-function(filePrefix,exp,t1,t2){
+sensor_save_df_main <-function(filePrefix,exp,chanGroupName,t1,t2){
 	
 filePath <- "/cluster/kuperberg/SemPrMM/MEG/results/sensor_level/R/"
-fileName <- paste(filePath,filePrefix,exp,'.',t1,"-",t2,".txt",sep="")
+fileName <- paste(filePath,filePrefix,".",exp,".",chanGroupName,".",t1,"-",t2,".txt",sep="")
 buffer<-read.table(fileName)
 erpData.all<-data.frame(subj=factor(buffer$V1),cond=factor(buffer$V2),elec=factor(buffer$V3),amp=buffer$V4,hemCode=factor(buffer$V5),antCode=factor(buffer$V6),midV=factor(buffer$V7),midH=factor(buffer$V8),elec9=factor(buffer$V9))
 
-outFile <- paste(filePath,filePrefix,exp,'.',t1,"-",t2,".df",sep="")
+outFile <- paste(filePath,filePrefix,".",exp,".",chanGroupName,t1,"-",t2,".df",sep="")
 save(erpData.all,file=outFile)
 }
