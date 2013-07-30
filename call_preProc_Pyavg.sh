c18ac2e6deacecdb4c5c6a898f84d25c964c26ec
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2013-07-30 10:15:51 -0400

    call_preProc_Pyavg.sh: minor changes

--- call_preProc_Pyavg.sh
+++ call_preProc_Pyavg.sh
@@ -4,7 +4,7 @@
 
 cd /autofs/cluster/kuperberg/SemPrMM/MEG/data/
 #foreach par ('BaleenHP' 'BaleenLP' 'ATLLoc' 'MaskedMM')
-#foreach par ('BaleenLP' 'MaskedMM')
+foreach par ('MaskedMM')
 echo $2
 foreach i (`cat /autofs/cluster/kuperberg/SemPrMM/MEG/scripts/function_inputs/$1.meg.all.txt`)   
          set subj = $1$i
