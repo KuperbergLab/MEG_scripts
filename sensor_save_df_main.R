e421b317908af38dbc2796ba4dc6759d3e7bb059
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2013-07-30 10:02:58 -0400

    fixed filename error in sensor_save_df_main.R

--- sensor_save_df_main.R
+++ sensor_save_df_main.R
@@ -10,6 +10,6 @@ if(chanGroupName == 'quad48Mid'){
 erpData.all<-data.frame(subj=factor(buffer$V6),subjGroup=factor(buffer$V5),exp=factor(buffer$V7),cond=factor(buffer$V8),elec=factor(buffer$V4),hemCode=factor(buffer$V1),antCode=factor(buffer$V2),chanLabel=factor(buffer$V3),amp=buffer$V9)
 }
 
-outFile <- paste(filePath,filePrefix,".",exp,".",chanGroupName,t1,"-",t2,".df",sep="")
+outFile <- paste(filePath,filePrefix,".",exp,".",chanGroupName,".",t1,"-",t2,".df",sep="")
 save(erpData.all,file=outFile)
 }
