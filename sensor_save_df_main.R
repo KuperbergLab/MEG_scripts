9c6e64d5de7490696bf5fdddd540b4d2c8d1eb22
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2013-09-02 15:35:24 -0400

    sensor_save_df_main.R: minor changes

--- sensor_save_df_main.R
+++ sensor_save_df_main.R
@@ -11,11 +11,19 @@ erpData.all<-data.frame(subj=factor(buffer$V6),subjGroup=factor(buffer$V5),exp=f
 }
 
 if(chanGroupName == 'gk_peripheral'){
-erpData.all<-data.frame(subj=factor(buffer$V6),subjGroup=factor(buffer$V5),exp=factor(buffer$V7),cond=factor(buffer$V8),elec=factor(buffer$V4),hemCode=factor(buffer$V1),antCode=factor(buffer$V2),chanLabel=factor(buffer$V3),amp=buffer$V9)
+erpData.all<-data.frame(subj=factor(buffer$V6),sGroup=factor(buffer$V5),exp=factor(buffer$V7),cond=factor(buffer$V8),elec=factor(buffer$V4),hem=factor(buffer$V1),APDist=factor(buffer$V2),chanLabel=factor(buffer$V3),amp=buffer$V9)
 }
 
 if(chanGroupName == 'gk_midline'){
-erpData.all<-data.frame(subj=factor(buffer$V5),subjGroup=factor(buffer$V4),exp=factor(buffer$V6),cond=factor(buffer$V7),elec=factor(buffer$V3),apCode=factor(buffer$V1),chanLabel=factor(buffer$V2),amp=buffer$V8)
+erpData.all<-data.frame(subj=factor(buffer$V5),sGroup=factor(buffer$V4),exp=factor(buffer$V6),cond=factor(buffer$V7),elec=factor(buffer$V3),APDist=factor(buffer$V1),chanLabel=factor(buffer$V2),amp=buffer$V8)
+}
+
+if(chanGroupName == 'elec_9'){
+erpData.all<-data.frame(subj=factor(buffer$V5),sGroup=factor(buffer$V4),exp=factor(buffer$V6),cond=factor(buffer$V7),elec=factor(buffer$V3),chanLabel=factor(buffer$V2),amp=buffer$V8)
+}
+
+if(chanGroupName == 'centropar_15'){
+erpData.all<-data.frame(subj=factor(buffer$V5),sGroup=factor(buffer$V4),exp=factor(buffer$V6),cond=factor(buffer$V7),elec=factor(buffer$V3),chanLabel=factor(buffer$V2),amp=buffer$V8)
 }
 
 
