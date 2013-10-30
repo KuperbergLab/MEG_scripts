9588008e243ef09388298046aad3d80917cd7171
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2013-10-30 10:13:09 -0400

    source_avgSTCinTime.m: minor changes

--- source_avgSTCinTime.m
+++ source_avgSTCinTime.m
@@ -1,7 +1,8 @@
 function source_avgSTCinTime(filePrefix, type,t1,t2)
 
 %%This script is simpler than most: it just takes a single stc file and
-%%averages it across a time-window. 
+%%averages it across a time-window.
+%%Example: source_avgSTCinTime('ga_ac.meg.all_MaskedMM_All_c3-c4M', 'spm', 300, 500)
 
 sample1 = round(t1/1.6666 + 61);
 sample2 = round(t2/1.6666 + 61);
@@ -13,15 +14,15 @@ for hemI = 1:2
     elseif hemI == 2
         hem = 'rh';
     end
-    
+    filename = strcat(filePrefix, '-',type,'-',hem,'.stc')
     stcStruct = mne_read_stc_file(strcat(filePrefix, '-',type,'-',hem,'.stc'));
     stcData = stcStruct.data;
     meanStcData = mean(stcData(:,sample1:sample2),2);
 
-    outFile = strcat(filePrefix,'_',int2str(t1),'-',int2str(t2),'-',type,'-',hem,'.stc');
+    outFile = strcat(filePrefix,'_',int2str(t1),'-',int2str(t2),'-',type,'-',hem,'.stc')
 
     newSTC = stcStruct;
     newSTC.data = meanStcData;
-
+     
     mne_write_stc_file(outFile,newSTC);
 end
