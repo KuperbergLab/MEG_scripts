8f8be4adfbc77678b248ef90bb586197c805f86c
ellen lau <ellenlau@nmr.mgh.harvard.edu>
2014-01-08 15:06:25 -0500

    preProc_setup.sh: no change

--- preProc_cov.sh
+++ preProc_cov.sh
@@ -27,33 +27,33 @@ python /cluster/kuperberg/SemPrMM/MEG/scripts/makeCovFiles.py $1  >>& $log
 echo "Computing covariance"  >>& $log
 date
 
-mne_process_raw \
- --raw ../$1_MaskedMMRun1_ssp_raw.fif \
- --raw ../$1_MaskedMMRun2_ssp_raw.fif \
- --cov ../cov/$1_MaskedMMRun1.cov \
- --cov ../cov/$1_MaskedMMRun2.cov \
- --gcov $1_MaskedMM_All-cov.fif \
- --projon --lowpass 20  >>& $log
- 
-mne_process_raw \
- --raw ../$1_BaleenLPRun1_ssp_raw.fif \
- --raw ../$1_BaleenLPRun2_ssp_raw.fif \
- --raw ../$1_BaleenLPRun3_ssp_raw.fif \
- --raw ../$1_BaleenLPRun4_ssp_raw.fif \
- --raw ../$1_BaleenHPRun1_ssp_raw.fif \
- --raw ../$1_BaleenHPRun2_ssp_raw.fif \
- --raw ../$1_BaleenHPRun3_ssp_raw.fif \
- --raw ../$1_BaleenHPRun4_ssp_raw.fif \
- --cov ../cov/$1_BaleenLPRun1.cov \
- --cov ../cov/$1_BaleenLPRun2.cov \
- --cov ../cov/$1_BaleenLPRun3.cov \
- --cov ../cov/$1_BaleenLPRun4.cov \
- --cov ../cov/$1_BaleenHPRun1.cov \
- --cov ../cov/$1_BaleenHPRun2.cov \
- --cov ../cov/$1_BaleenHPRun3.cov \
- --cov ../cov/$1_BaleenHPRun4.cov \
- --gcov $1_Baleen_All-cov.fif \
- --projon --lowpass 20  >>& $log
+# mne_process_raw \
+#  --raw ../$1_MaskedMMRun1_ssp_raw.fif \
+#  --raw ../$1_MaskedMMRun2_ssp_raw.fif \
+#  --cov ../cov/$1_MaskedMMRun1.cov \
+#  --cov ../cov/$1_MaskedMMRun2.cov \
+#  --gcov $1_MaskedMM_All-cov.fif \
+#  --projon --lowpass 20  >>& $log
+#  
+# mne_process_raw \
+#  --raw ../$1_BaleenLPRun1_ssp_raw.fif \
+#  --raw ../$1_BaleenLPRun2_ssp_raw.fif \
+#  --raw ../$1_BaleenLPRun3_ssp_raw.fif \
+#  --raw ../$1_BaleenLPRun4_ssp_raw.fif \
+#  --raw ../$1_BaleenHPRun1_ssp_raw.fif \
+#  --raw ../$1_BaleenHPRun2_ssp_raw.fif \
+#  --raw ../$1_BaleenHPRun3_ssp_raw.fif \
+#  --raw ../$1_BaleenHPRun4_ssp_raw.fif \
+#  --cov ../cov/$1_BaleenLPRun1.cov \
+#  --cov ../cov/$1_BaleenLPRun2.cov \
+#  --cov ../cov/$1_BaleenLPRun3.cov \
+#  --cov ../cov/$1_BaleenLPRun4.cov \
+#  --cov ../cov/$1_BaleenHPRun1.cov \
+#  --cov ../cov/$1_BaleenHPRun2.cov \
+#  --cov ../cov/$1_BaleenHPRun3.cov \
+#  --cov ../cov/$1_BaleenHPRun4.cov \
+#  --gcov $1_Baleen_All-cov.fif \
+#  --projon --lowpass 20  >>& $log
  
 
 if ( -e ../$1_AXCPTRun1_raw.fif ) then
